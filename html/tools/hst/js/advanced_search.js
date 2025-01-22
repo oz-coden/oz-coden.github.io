ad205995a118d14ee90ae90294003677d48b18f2
oz-coden <163804408+oz-coden@users.noreply.github.com>
2025-01-23 00:57:19 +0900

    Add files via upload

--- html/tools/hst/js/advanced_search.js
+++ html/tools/hst/js/advanced_search.js
@@ -1,24 +1,44 @@
 function advancedSearchPress() {
     let search_key_all_keywords = document.getElementById("search_key_all_keywords").value;
-    let search_key_all_complete = document.getElementById("search_key_all_complete").value;
-    let search_key_any_keywords = document.getElementById("search_key_any_keywords").value;
-    let search_key_not_keywords = document.getElementById("search_key_not_keywords").value;
-    let search_key_site_domain_type = document.getElementById("search_key_site_domain_type").value;
+    let b_search_key_all_complete = document.getElementById("search_key_all_complete").value;
+    let b_search_key_any_keywords = document.getElementById("search_key_any_keywords").value;
+    let b_search_key_not_keywords = document.getElementById("search_key_not_keywords").value;
+    let b_search_key_site_domain_type = document.getElementById("search_key_site_domain_type").value;
 
-    if (search_key_all_keywords == "" && search_key_all_complete == "" && search_key_any_keywords == "" && search_key_not_keywords == "") {
+    if (search_key_all_keywords == "" && b_search_key_all_complete == "" && b_search_key_any_keywords == "" && b_search_key_not_keywords == "") {
         return;
     }
 
-    search_key_all_complete = "\"" + search_key_all_complete + "\"";
+    let search_key_all_complete = "\"" + b_search_key_all_complete + "\"";
 
-    search_key_any_keywords = search_key_any_keywords.split(" ").join(" OR ").split("　").join(" OR ");
+    let search_key_any_keywords = b_search_key_any_keywords.split(" ").join(" OR ").split("　").join(" OR ");
 
-    search_key_not_keywords = ("-" + search_key_not_keywords).split(" ").join(" -").split("　").join(" -");
+    let search_key_not_keywords = ("-" + b_search_key_not_keywords).split(" ").join(" -").split("　").join(" -");
 
-    search_key_site_domain_type = "site:" + search_key_site_domain_type.replace(" ", "").replace("　", "");
+    let search_key_site_domain_type = "site:" + b_search_key_site_domain_type.replace(" ", "").replace("　", "");
 
     let engine = document.getElementById('search_engine').value;
 
-    const url = "https://" + engine + encodeURIComponent(search_key_all_keywords + " " + search_key_all_complete + " " + search_key_any_keywords + " " + search_key_not_keywords + " " + search_key_site_domain_type);
+    let search_key = "";
+
+    if (search_key_all_keywords != "") {
+        search_key = search_key + search_key_all_keywords + " ";
+    }
+    if (b_search_key_all_complete != "") {
+        search_key = search_key + search_key_all_complete + " ";
+    }
+    if (b_search_key_any_keywords != "") {
+        search_key = search_key + search_key_any_keywords + " ";
+    }
+    if (b_search_key_not_keywords != "") {
+        search_key = search_key + search_key_not_keywords + " ";
+    }
+    if (b_search_key_site_domain_type != "") {
+        search_key = search_key + search_key_site_domain_type;
+    }
+
+    search_key.trimEnd(" ");
+
+    const url = "https://" + engine + encodeURIComponent(search_key);
     window.open(url, '_blank');
 }
